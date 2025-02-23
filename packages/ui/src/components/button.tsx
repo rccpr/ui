bc45f27f3faf862ad64dd2a01c9942b27678156e
Gabriel Pérez <gabriel@garox.org>
2025-02-23 16:18:49 -0300

    test again

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -1,31 +1,46 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import type {
+  ClassProp,
+  ClassValue,
+  StringToBoolean,
+} from "class-variance-authority/types";
 
 import { cn } from "../lib/utils";
+const variants = {
+  variant: {
+    default: "bg-primary text-primary-foreground hover:bg-primary/90",
+    destructive:
+      "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+    outline:
+      "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+    secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+    ghost: "hover:bg-accent hover:text-accent-foreground",
+    link: "text-primary underline-offset-4 hover:underline",
+  },
+  size: {
+    default: "h-10 px-4 py-2",
+    sm: "h-9 rounded-md px-3",
+    lg: "h-11 rounded-md px-8",
+    icon: "h-10 w-10",
+  },
+} as const;
 
-const buttonVariants = cva(
+type ConfigSchema = Record<string, Record<string, ClassValue>>;
+
+type ConfigVariants<T extends ConfigSchema> = {
+  [Variant in keyof T]?: StringToBoolean<keyof T[Variant]> | null | undefined;
+};
+
+type ButtonVariants = (
+  props: (ConfigVariants<typeof variants> & ClassProp) | undefined
+) => string;
+
+const buttonVariants: ButtonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
-    variants: {
-      variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
-      },
-    },
+    variants,
     defaultVariants: {
       variant: "default",
       size: "default",
@@ -33,22 +48,9 @@ const buttonVariants = cva(
   }
 );
 
-type ButtonVariantsProps = {
-  variant?:
-    | "default"
-    | "destructive"
-    | "outline"
-    | "secondary"
-    | "ghost"
-    | "link"
-    | null
-    | undefined;
-  size?: "default" | "sm" | "lg" | "icon" | null | undefined;
-};
-
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    ButtonVariantsProps {
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
@@ -67,4 +69,4 @@ const Button: React.FC<ButtonProps> = React.forwardRef<
 });
 Button.displayName = "Button";
 
-export { Button };
+export { Button, buttonVariants };
